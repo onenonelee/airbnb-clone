6a28cfa55f94dd28bc94595aca251851196e4f11
one_n_onelee <leeonechang@icloud.com>
2020-05-24 02:06:20 +0900

    #3 Initialize User App

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)  # Register My User model on the admin panel
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    # list_display makes the columns on the admin panel
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+
+    # list_display makes a sidebar on the admin panel with those filters
+    list_filter = ("language", "currency", "superhost")
+
+    # fildsets makes things that I can modify on the admin user panel
+    # UserAdmin already has things, so I add up Custom Profile to UserAdmin
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
