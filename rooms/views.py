5e165ca3095c2e69e87dec50dc873e653071fd91
one_n_onelee <leeonechang@icloud.com>
2020-05-25 00:10:37 +0900

    #11.1 paginating on python

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,19 @@
+from math import ceil
 from django.shortcuts import render
-from . import models as room_models
+from . import models
 
 
 def all_rooms(request):
-    all_rooms = room_models.Room.objects.all()
-    return render(request, "rooms/all_rooms.html", context={"AR": all_rooms})
+    page = request.GET.get("page", 1)
+    print(page)
+    page = int(page or 1)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    end_page = ceil(models.Room.objects.count() / page_size)
+    return render(
+        request,
+        "rooms/all_rooms.html",
+        context={"AR": all_rooms, "current_page": page, "end_page": end_page},
+    )
