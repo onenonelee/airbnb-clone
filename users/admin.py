dbf744de5577667515c35b2ec17e647a40942770
one_n_onelee <leeonechang@icloud.com>
2020-05-24 21:44:23 +0900

    #9 Seeding Database

--- users/admin.py
+++ users/admin.py
@@ -9,10 +9,21 @@ class CustomUserAdmin(UserAdmin):
     """ Custom User Admin """
 
     # list_display makes the columns on the admin panel
-    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )
 
-    # list_display makes a sidebar on the admin panel with those filters
-    list_filter = ("language", "currency", "superhost")
+    # list_filter makes a sidebar on the admin panel with those filters
+    list_filter = UserAdmin.list_filter + ("superhost",)
 
     # fildsets makes things that I can modify on the admin user panel
     # UserAdmin already has things, so I add up Custom Profile to UserAdmin
@@ -31,19 +42,4 @@ class CustomUserAdmin(UserAdmin):
                 )
             },
         ),
-    )
-
-    list_filter = UserAdmin.list_filter + ("superhost",)
-
-    list_display = (
-        "username",
-        "first_name",
-        "last_name",
-        "email",
-        "is_active",
-        "language",
-        "currency",
-        "superhost",
-        "is_staff",
-        "is_superuser",
     )
