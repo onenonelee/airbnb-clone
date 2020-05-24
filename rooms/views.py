0eac715a0307bf51a5c6d3d020a47059d136dfa0
one_n_onelee <leeonechang@icloud.com>
2020-05-24 23:26:13 +0900

    #10 views & urls

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import models as room_models
 
-# Create your views here.
+
+def all_rooms(request):
+    all_rooms = room_models.Room.objects.all()
+    return render(request, "rooms/all_rooms.html", context={"AR": all_rooms})
